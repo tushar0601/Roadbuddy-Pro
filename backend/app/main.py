84a574f3e2ef72e94a329f6b48db852b3ce692f0
Tushar Kumar <tusharjan6@gmail.com>
2026-04-02 01:58:56 +0530

    commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,10 +7,7 @@ from app.api.v1.router import api_router
 app = FastAPI(title="RoadBuddy API")
 
 
-origins = [
-    "http://localhost:3000",  
-    "http://127.0.0.1:3000",
-]
+origins = [""]
 
 app.add_middleware(
     CORSMiddleware,
